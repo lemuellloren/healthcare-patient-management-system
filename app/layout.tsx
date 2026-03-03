ae2b16f0c0c8fa5749ba521491924ae036237383
lemuellloren <lempakz@yahoo.com>
2026-03-03 11:23:48 +0800

    Add admin dashboard and appointment management features and sentry

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={cn(
           'min-h-screen bg-dark-300 font-sans antialiased',
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         <ThemeProvider
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ec3c7721f31047f6986b4675087c99ad@o4510978331967488.ingest.us.sentry.io/4510978348154880",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
