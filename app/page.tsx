da64461ea0d07399aaadeaa4d1d473d6f6906bfd
lemuellloren <lempakz@yahoo.com>
2026-03-03 09:33:58 +0800

    Add PasskeyModal component for admin access verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { PatientForm } from '@/components/forms/PatientForm';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+import { PatientForm } from '@/components/forms/PatientForm';
+import { PasskeyModal } from '@/components/PasskeyModal';
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === 'true';
+
   return (
     <div className='flex h-screen max-h-screen'>
-      {/* TODO: OTP Verification | PasskeyModal  */}
+      {isAdmin && <PasskeyModal />}
+
       <section className='remove-scrollbar container my-auto'>
         <div className='sub-container max-w-[496px]'>
           <Image
@@ -15,10 +20,12 @@ export default function Home() {
             alt='patient'
             className='mb-12 h-10 w-fit'
           />
+
           <PatientForm />
+
           <div className='text-14-regular mt-20 flex justify-between'>
             <p className='justify-items-end text-dark-600 xl:text-left'>
-              &copy; 2024 CarePulse
+              © 2024 CarePluse
             </p>
             <Link href='/?admin=true' className='text-green-500'>
               Admin
@@ -26,6 +33,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       <Image
         src='/assets/images/onboarding-img.png'
         height={1000}
@@ -35,4 +43,6 @@ export default function Home() {
       />
     </div>
   );
-}
+};
+
+export default Home;
